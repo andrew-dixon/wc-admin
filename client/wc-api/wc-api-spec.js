a9e14fc49c271e4bf67b4b1b87cb515474399f29
Kevin Killingsworth <kevin@redfenix.com>
2018-12-10 16:13:30 -0600

    wc-api: Add user meta for UI preferences
    This adds wc-api code to store UI preferences and provides one example
    of updating the revenue table to save column preferences.

--- client/wc-api/wc-api-spec.js
+++ client/wc-api/wc-api-spec.js
@@ -7,14 +7,19 @@ import notes from './notes';
 import orders from './orders';
 import reportItems from './reports/items';
 import reportStats from './reports/stats';
+import user from './user';
 
 function createWcApiSpec() {
 	return {
+		mutations: {
+			...user.mutations,
+		},
 		selectors: {
 			...notes.selectors,
 			...orders.selectors,
 			...reportItems.selectors,
 			...reportStats.selectors,
+			...user.selectors,
 		},
 		operations: {
 			read( resourceNames ) {
@@ -23,7 +28,11 @@ function createWcApiSpec() {
 					...orders.operations.read( resourceNames ),
 					...reportItems.operations.read( resourceNames ),
 					...reportStats.operations.read( resourceNames ),
+					...user.operations.read( resourceNames ),
 				];
+			},
+			update( resourceNames, data ) {
+				return [ ...user.operations.update( resourceNames, data ) ];
 			},
 		},
 	};
